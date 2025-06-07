5511d1ea3947a101ddc0fedda80c860a069ee0a9
Alonso <alonso.s2k5@gmail.com>
2025-06-07 17:51:10 -0500

    Update next.config.ts
    ,

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    allowedDevOrigins: [
+    'https://pc3-dawa.onrender.com', // o el dominio desde donde accedes
+  ],
 };
 
 export default nextConfig;
